5ec6ced04523c48ff9762532103e92f16e92296b
caio peternela <caiopeternela.dev@gmail.com>
2023-11-25 10:44:27 -0300

    Update env.clj

--- src/url_shortener/env.clj
+++
@@ -1,6 +0,0 @@
-(ns url-shortener.env)
-
-(def region  "us-east-1")
-(def datomic-endpoint "https://fe8gqooi4i.execute-api.us-east-1.amazonaws.com")
-(def system-name "datomic-url-shortener")
-(def creds-profile "default")
